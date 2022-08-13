42033d0f6c34ab1659f191259009283a99e33f8c
Mong9 <bean326@naver.com>
2022-08-14 01:08:30 +0900

    ✅ feat: localStorage에 메뉴 상태를 저장하여 관리하기 - 메뉴 추가하기

--- src/js/index.js
+++ src/js/index.js
@@ -4,21 +4,21 @@
 // - [O] localStorage에 있는 데이터를 읽어온다.
 
 // TODO 카테고리별 메뉴판 관리
-// - [O] 에스프레소 메뉴판 관리
-// - [O] 프라푸치노 메뉴판 관리
-// - [O] 블렌디드 메뉴판 관리
-// - [O] 티바나 메뉴판 관리
-// - [O] 디저트 메뉴판 관리
+// - [] 에스프레소 메뉴판 관리
+// - [] 프라푸치노 메뉴판 관리
+// - [] 블렌디드 메뉴판 관리
+// - [] 티바나 메뉴판 관리
+// - [] 디저트 메뉴판 관리
 
 // TODO 페이지 접근시 최초 데이터 Read & Rendering
-// - [O] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
-// - [O] 에스프레소 메뉴를 페이지에 그려준다.
+// - [] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
+// - [] 에스프레소 메뉴를 페이지에 그려준다.
 
 // TODO 품절 상태 관리
-// - [O] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-// - [O] 품절 버튼을 추가한다.
-// - [O] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
-// - [O] 클릭이벤트에서 가장가까운 li태그의 class 속성 값에 sold-out을 추가한다.
+// - [] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [] 품절 버튼을 추가한다.
+// - [] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+// - [] 클릭이벤트에서 가장가까운 li태그의 class 속성 값에 sold-out을 추가한다.
 
 
 const $ = (selector) => document.querySelector(selector);
@@ -33,6 +33,8 @@ const store = {
 };
 
 function App() {
+  this.menu = [];
+
   const updateMenuCount= () => {
     const menuCount = $('.menu-list').querySelectorAll('li').length;
     $('.menu-count').innerHTML = `총 ${menuCount}개`;
@@ -44,10 +46,13 @@ function App() {
       return;
     }
     const espressoMenuName = $('.input-field').value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return;
-      `<li class="menu-list-item d-flex items-center py-2">
-              <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+    this.menu.push({name: espressoMenuName});
+    store.setLocalStorage(this.menu);
+    const template = this.menu
+      .map((item) => {
+        return;
+        `<li class="menu-list-item d-flex items-center py-2">
+              <span class="w-100 pl-2 menu-name">${item.name}</span>
               <button
               type="button"
               class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -61,11 +66,10 @@ function App() {
               삭제
               </button>
               </li>`;
-    };
-    $('.menu-list').insertAdjacentHTML(
-      'beforeEnd',
-      menuItemTemplate(espressoMenuName)
-    );
+      })
+      .join('');
+
+    $('.menu-list').innerHTML = template
     updateMenuCount();
     $('.input-filed').value = '';
   };
@@ -75,7 +79,6 @@ function App() {
     const updatedMenuName = prompt('메뉴명을 수정하세요', menuName.innerText);
     $menuName.innerText = updatedMenuName;
   }
-
 
   const removeMenuName = (e) => {
     if (confirm('정말 삭제하시겠습니까?')) {
