2eaec1bdd5c6980f441878b187fef2e5f008200f
Mong9 <bean326@naver.com>
2022-08-13 23:56:24 +0900

    ✅ feat : 삭제 버튼을 눌러 'confirm'창에서 메뉴삭제 | 브라우저 인터페이스

--- src/js/index.js
+++ src/js/index.js
@@ -9,23 +9,34 @@
 // - [O] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 // TODO 메뉴 수정
-// - [] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하느 모달창이 뜬다.
-// - [] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+// - [O] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하느 모달창이 뜬다.
+// - [O] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
-// - [] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-// - [] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [O] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [O] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [O] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
 const $ = (selector) => document.querySelector(selector);
 
 
 function App() {
+  const updateMenuCount= () => {
+    const menuCount = $('.menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerHTML = `총 ${menuCount}개`;
+  }
+
   $('.menu-list-item').addEventListener('click', (e) => {
     if (e.target.classList.contains('menu-edit-button')) {
       const $menuName = e.target.closest('li').querySelector('.menu-name');
       const updatedMenuName = prompt('메뉴명을 수정하세요', menuName.innerText);
       $menuName.innerText = updatedMenuName;
+    } 
+    if(e.target.classList.contains('menu-remove-button')) {
+      if(confirm('정말 삭제하시겠습니까?')) {
+        e.target.closest('li').remove();
+        updateMenuCount();
+      }
     }
   });
 
@@ -63,8 +74,7 @@ function App() {
             'beforeEnd',
             menuItemTemplate(espressoMenuName)
           );
-          const menuCount = $('.menu-list').querySelectorAll('li').length;
-          $('.menu-count').innerHTML = `총 ${menuCount}개`;
+            updateMenuCount()
           $('.input-filed').value = '';
     
         }
