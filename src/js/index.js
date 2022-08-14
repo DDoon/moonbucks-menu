6e66a0a35a0c40fd23c262fc949bea522b17da5e
Mong9 <bean326@naver.com>
2022-08-14 13:28:36 +0900

    ✅ feat: localStorage에 메뉴 상태를 저장하여 관리하기 - 메뉴 수정하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 // step2 요구사항 구현을 위한 전략
 // TODO localStorage Read & Write
 // - [O] localStorage에 데이터를 저장한다.
+// - [O] 메뉴를 추가할 때
+// - [] 메뉴를 수정할 때
+// - [] 메뉴를 삭제할 때
 // - [O] localStorage에 있는 데이터를 읽어온다.
-
+ 
 // TODO 카테고리별 메뉴판 관리
 // - [] 에스프레소 메뉴판 관리
 // - [] 프라푸치노 메뉴판 관리
@@ -49,10 +52,10 @@ function App() {
     this.menu.push({name: espressoMenuName});
     store.setLocalStorage(this.menu);
     const template = this.menu
-      .map((item) => {
-        return;
-        `<li class="menu-list-item d-flex items-center py-2">
-              <span class="w-100 pl-2 menu-name">${item.name}</span>
+      .map((menuItem, index) => {
+        return
+        `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+              <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
               <button
               type="button"
               class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -75,8 +78,11 @@ function App() {
   };
 
   const updateMenuName = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId
     const $menuName = e.target.closest('li').querySelector('.menu-name');
     const updatedMenuName = prompt('메뉴명을 수정하세요', menuName.innerText);
+    this.menu[menuId].name = updatedMenuName
+    store.setLocalStorage(this.menu);
     $menuName.innerText = updatedMenuName;
   }
 
